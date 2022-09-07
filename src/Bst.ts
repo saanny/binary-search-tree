c15905e7083ae05a6e8b26b2925012848a54fb28
amir ahmadi <amirahmadidev@gmail.com>
2022-09-07 18:20:10 +0430

    init

---
+++ src/Bst.ts
@@ -0,0 +1,60 @@
+export class BST {
+    value: number;
+    left: BST | null;
+    right: BST | null;
+
+    constructor(value: number) {
+        this.value = value;
+        this.left = null;
+        this.right = null;
+    }
+
+    insert(value: number) {
+        let currentNode: BST = this;
+
+        const trueFlag = true;
+        while (trueFlag) {
+            if (value < currentNode.value) {
+                if (currentNode.left === null) {
+                    currentNode.left = new BST(value);
+                    break;
+                } else {
+                    currentNode = currentNode.left;
+                }
+            } else {
+                if (currentNode.right === null) {
+                    currentNode.right = new BST(value)
+                    break;
+                } else {
+                    currentNode = currentNode.right;
+                }
+
+            }
+        }
+
+        return this;
+    }
+
+    contains(value: number) {
+        return false;
+    }
+}
+
+// const BstObject = new BST(10)
+// .insert(5)
+// .insert(15)
+// .insert(2)
+// .insert(5)
+// .insert(1)
+// .insert(5)
+// .insert(1)
+// .insert(3)
+// .insert(13)
+// .insert(22)
+// .insert(25)
+// .insert(12)
+// .insert(14)
+
+// BstObject.contains(15)
+
+// console.log(BstObject);
---
+++ src/bst.spec.ts
@@ -0,0 +1,15 @@
+// import { BST } from './Bst';
+
+// describe('Binary Search Tree', () => {
+
+
+//     test('it shoud return bst', () => {
+
+
+      
+
+//     });
+
+// });
+
+
